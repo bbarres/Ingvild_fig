20ebf5c613afee3c9493f2a75fad3559156cebd5
bbarres <benoit.barres@gmail.com>
2020-07-28 15:43:09 +0200

    adding the pdf files combining all the plot

--- Ingvild_distrfluidigm_plot.R
+++ Ingvild_distrfluidigm_plot.R
@@ -47,13 +47,13 @@ for (i in 3:(dim(logstattout)[2]-1)) {
     xlab("individus") + ylab("log2(ddct)") + 
     labs(fill = "Phenotype") + 
     ggtitle(colnames(logstattout)[i],
-            subtitle="pval : ajouter ligne tableau") + #here something to adjust
+            subtitle="pval : ajouter ligne tableau") + #here for subtitle
     theme(plot.title = element_text(hjust = 0.5)) + 
     theme(plot.subtitle = element_text(hjust = 0.5)) + 
     ylim(-10,10)
   
-  ggsave(file=paste("./output/log(",colnames(logstattout)[i],").png",sep=""),
-         width=11,height=8)
+  ggsave(file=paste("./output/pop-log(",colnames(logstattout)[i],").png",
+                    sep=""),width=11,height=8)
   
 }
 
@@ -62,6 +62,39 @@ for (i in 3:(dim(logstattout)[2]-1)) {
 #one pdf file, ordered by population then by ascending log2 
 ##############################################################################/
 
+pdf(file="./output/all-pop-log.pdf",width=11,height=8)
+
+for (i in 3:(dim(logstattout)[2]-1)) {
+  
+  datadd<-logstattout[,c(1,2,i,90)]
+  #replacing missing data by "0"
+  datadd[is.na(datadd[,3]),3]<-0
+  #here you determine the order of the data
+  datadd<-arrange(datadd,popula,get(colnames(logstattout)[i]))
+  datadd$ordina<-seq.int(nrow(datadd))
+  print(
+    ggplot(data=datadd,aes(x=reorder(sorteddatatoutpheno,ordina),
+                           y=get(colnames(logstattout)[i]),
+                           fill=sorteddatatoutpheno_1),
+           position=position_dodge()) + 
+      geom_bar(stat="identity") +
+      theme_minimal() +
+      
+      theme(axis.text.x = element_text(angle=90,size=12,hjust=0,vjust=0.5,
+                                       colour=colopop[datadd$popula])) + 
+      scale_fill_manual(values=c("#FF0000","#33CC33")) + 
+      theme(legend.position="bottom") + 
+      xlab("individus") + ylab("log2(ddct)") + 
+      labs(fill = "Phenotype") + 
+      ggtitle(colnames(logstattout)[i],
+              subtitle="pval : ajouter ligne tableau") + #here for subtitle
+      theme(plot.title = element_text(hjust = 0.5)) + 
+      theme(plot.subtitle = element_text(hjust = 0.5)) + 
+      ylim(-10,10)
+  )
+}
+
+dev.off()
 
 
 ##############################################################################/
@@ -90,7 +123,7 @@ for (i in 3:(dim(logstattout)[2]-1)) {
     xlab("individus") + ylab("log2(ddct)") + 
     labs(fill = "Phenotype") + 
     ggtitle(colnames(logstattout)[i],
-            subtitle="pval : ajouter ligne tableau") + #here something to adjust
+            subtitle="pval : ajouter ligne tableau") + #here for subtitle
     theme(plot.title = element_text(hjust = 0.5)) + 
     theme(plot.subtitle = element_text(hjust = 0.5)) + 
     ylim(-10,10)
@@ -99,6 +132,45 @@ for (i in 3:(dim(logstattout)[2]-1)) {
          width=11,height=8)
   
 }
+
+
+##############################################################################/
+#one pdf file, ordered by ascending log2 
+##############################################################################/
+
+pdf(file="./output/all-log.pdf",width=11,height=8)
+
+for (i in 3:(dim(logstattout)[2]-1)) {
+  
+  datadd<-logstattout[,c(1,2,i,90)]
+  #replacing missing data by "0"
+  datadd[is.na(datadd[,3]),3]<-0
+  #here you determine the order of the data
+  datadd<-arrange(datadd,get(colnames(logstattout)[i]))
+  datadd$ordina<-seq.int(nrow(datadd))
+  print(
+    ggplot(data=datadd,aes(x=reorder(sorteddatatoutpheno,ordina),
+                           y=get(colnames(logstattout)[i]),
+                           fill=sorteddatatoutpheno_1),
+           position=position_dodge()) + 
+      geom_bar(stat="identity") +
+      theme_minimal() + 
+      
+      theme(axis.text.x = element_text(angle=90,size=12,hjust=0,vjust=0.5,
+                                       colour=colopop[datadd$popula])) + 
+      scale_fill_manual(values=c("#FF0000","#33CC33")) + 
+      theme(legend.position="bottom") + 
+      xlab("individus") + ylab("log2(ddct)") + 
+      labs(fill = "Phenotype") + 
+      ggtitle(colnames(logstattout)[i],
+              subtitle="pval : ajouter ligne tableau") + #here for subtitle
+      theme(plot.title = element_text(hjust = 0.5)) + 
+      theme(plot.subtitle = element_text(hjust = 0.5)) + 
+      ylim(-10,10)
+  )
+}
+
+dev.off()
 
 
 ##############################################################################/
